b24d2aba744893540eda0993e19396452634a7af
Yanis Guenane <yguenane@gmail.com>
2013-09-02 08:48:35 -0400

    Enable a check before resetting the secrets.tdb ldap bind dn password
    A check is now applied before running the smbpasswd -w to verify that
    we are not resetting the password to it's same value, avoiding useless
    reboot of the service. The tdb-tools package needed to be installed so
    it could be done

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,7 @@ class samba::params {
   case $::osfamily {
     'RedHat': {
       $service = [ 'smb', 'nmb' ]
+      $secretstdb = '/var/lib/samba/private/secrets.tdb'
     }
     'Debian': {
       if $::operatingsystem == 'Ubuntu' {
@@ -12,9 +13,11 @@ class samba::params {
       } else {
         $service = [ 'samba' ]
       }
+      $secretstdb = '/var/lib/samba/secrets.tdb'
     }
     default: {
       $service = [ 'samba' ]
+      $secretstdb = '/usr/local/samba/private/secrets.tdb'
     }
   }
 

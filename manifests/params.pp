f06ee9b65a064261c097ad2bb5076d79075ae85a
fess <fess@laptop>
2014-03-30 19:35:51 +0900

    add support freebsd

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,13 @@
 # Class: samba::params
-#
+# vim: sts=2 ts=2 sw=2 expandtab autoindent
 class samba::params {
 
   case $::osfamily {
     'RedHat': {
-      $service = [ 'smb', 'nmb' ]
-      $secretstdb = '/var/lib/samba/private/secrets.tdb'
+      $service = [ 'smb', 'nmb' ],
+      $secretstdb = '/var/lib/samba/private/secrets.tdb',
+      $samba_config_path = '/etc/smb.conf',
+      $package_name = 'samba',
     }
     'Debian': {
       if $::operatingsystem == 'Ubuntu' {
@@ -14,10 +16,21 @@ class samba::params {
         $service = [ 'samba' ]
       }
       $secretstdb = '/var/lib/samba/secrets.tdb'
+      $samba_config_path = '/etc/smb.conf',
+      $package_name = 'samba',
+    }
+    'Freebsd': {
+        $service = [ 'samba' ],
+        $samba_config_path = '/usr/local/etc/smb.conf',
+        $package_name = 'samba36',
+      }
+      $secretstdb = '/var/lib/samba/secrets.tdb'
     }
     default: {
       $service = [ 'samba' ]
       $secretstdb = '/usr/local/samba/private/secrets.tdb'
+      $samba_config_path = '/etc/smb.conf',
+      $package_name = 'samba',
     }
   }
 
